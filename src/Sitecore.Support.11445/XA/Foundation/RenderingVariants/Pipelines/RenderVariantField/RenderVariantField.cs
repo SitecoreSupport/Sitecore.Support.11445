2dc05fc71d8a3d8fa7793073fa8bf6bd20cf3ca5
DK\NPA <npa@sitecore.net>
2018-02-06 17:52:51 +0200

    Native code

---
+++ src/Sitecore.Support.11445/XA/Foundation/RenderingVariants/Pipelines/RenderVariantField/RenderVariantField.cs
@@ -0,0 +1,17 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.XA.Foundation.RenderingVariants.Fields;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Sitecore.Support.XA.Foundation.RenderingVariants.Pipelines.RenderVariantField
+{
+  public class RenderVariantField: Sitecore.XA.Foundation.RenderingVariants.Pipelines.RenderVariantField.RenderVariantField
+  {
+    #region patch
+    protected override bool IsEmptyFieldToRender(VariantField variantField, Item item) =>
+    (!variantField.RenderIfEmpty && string.IsNullOrWhiteSpace(item[variantField.FieldName]));
+    #endregion
+  }
+}
